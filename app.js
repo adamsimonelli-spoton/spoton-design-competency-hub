dcf59f66f3eae344a0c32239d2c96c7f405b5481
Adam Simonelli <adam.simonelli@spoton.com>
2026-03-24 12:45:35 -0400

    Quick Win modal: external link icon dark gray

--- app.js
+++ app.js
@@ -4876,7 +4876,7 @@ function render() {
                         ${r.author ? `<span class="insight-resource-author">${escHtml(r.author)}</span>` : ''}
                       </div>
                       <span class="insight-resource-type">${escHtml(r.type)}</span>
-                      <svg width="14" height="14" viewBox="0 0 256 256" fill="currentColor" style="flex-shrink:0;opacity:.4;margin-left:4px" aria-hidden="true"><path d="M224,104a8,8,0,0,1-16,0V59.32l-82.34,82.34a8,8,0,0,1-11.32-11.32L196.68,48H152a8,8,0,0,1,0-16h64a8,8,0,0,1,8,8Zm-40,24a8,8,0,0,0-8,8v72H48V80h72a8,8,0,0,0,0-16H48A16,16,0,0,0,32,80V208a16,16,0,0,0,16,16H176a16,16,0,0,0,16-16V136A8,8,0,0,0,184,128Z"/></svg>
+                      <svg width="14" height="14" viewBox="0 0 256 256" fill="currentColor" style="flex-shrink:0;color:#4B5563;margin-left:4px" aria-hidden="true"><path d="M224,104a8,8,0,0,1-16,0V59.32l-82.34,82.34a8,8,0,0,1-11.32-11.32L196.68,48H152a8,8,0,0,1,0-16h64a8,8,0,0,1,8,8Zm-40,24a8,8,0,0,0-8,8v72H48V80h72a8,8,0,0,0,0-16H48A16,16,0,0,0,32,80V208a16,16,0,0,0,16,16H176a16,16,0,0,0,16-16V136A8,8,0,0,0,184,128Z"/></svg>
                     </a>
                   `).join('')}
                 </div>
